e5ebd18d63556528e35e5d2e884477556108a2f0
Vaishnavi git <vchandupatla2001@gmail.com>
2024-02-18 00:30:15 +0530

    suffix product (2)

--- suffix_product.py
+++ suffix_product.py
@@ -2,7 +2,7 @@ def suffix_product(l,n):
     suff=[1]*n
     suff[-1]=l[-1] # initialise last index of suffix array
     for i in range(n-2,-1,-1): # iterate from right to left start from (n-1) t0
-        print(i)
+        #print(i)
         suff[i]=suff[i+1]*l[i]
     return suff
 l=list(map(int, input("enter list: ").strip().split(" ")))
