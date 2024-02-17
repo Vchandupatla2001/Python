2027049383f49cb0542e52559d93daa696a8841f
Vaishnavi git <vchandupatla2001@gmail.com>
2024-02-18 00:27:10 +0530

    Longest common prefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,12 @@
+def longest_common_prefix(strs):
+    shortest=min(strs,key=len)
+    res=""
+    for i in range(len(shortest)):
+        curr=strs[0][i]
+        if (all(words[i]==curr for words in strs)):
+            res=res+(str(curr))
+        else:
+            break
+    return res
+strs=['flower',"fllight","flight"]
+print(longest_common_prefix(strs))
